f806cc114c3873a00164e98bba12a418e5c0151f
Gianinna <gianinna.marquez.contractor@gdevtools.com>
2021-07-30 23:09:20 -0300

    Testing mounted methods and calling API

--- src/components/CounterManagement/index.tsx
+++ src/components/CounterManagement/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Component } from 'react';
 import { CounterManagementProps, CounterManagementState } from './interface';
+import axios from 'axios';
  
 class CounterManagement extends React.Component<CounterManagementProps, CounterManagementState> {
     constructor(props: CounterManagementProps) {
         super(props);
         this.state = { 
-            counter: 0
+            counter: 0,
+            users: []
         };
+        console.log("constructor");
     }
 
     handleAddClick = () => {
@@ -18,9 +21,29 @@ class CounterManagement extends React.Component<CounterManagementProps, CounterM
         this.setState({ counter: this.state.counter - 1 })
     }
 
+    static getDerivedStateFromProps(props: CounterManagementProps) {
+        console.log("getDerivedStateFromProps")
+        return null
+    }
+
+    clickWindow = () => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+
+    componentDidMount() {
+        axios.get('https://pokeapi.co/api/v2/pokemon')
+            .then(response => {
+                const data = response.data.results;
+                const users = data.map((userData: any) => userData.name);
+                this.setState({ users });
+            })
+        window.addEventListener('click', this.clickWindow)
+    }    
+
     render() { 
+        console.log("render");
         const { ownerName } = this.props;
-        const { counter } = this.state;
+        const { counter, users } = this.state;
         return (  
             <div>
                 <h1>Counter Management</h1>
@@ -28,6 +51,9 @@ class CounterManagement extends React.Component<CounterManagementProps, CounterM
                 <h1>{counter}</h1>
                 <button onClick={this.handleAddClick}> Add </button>
                 <button onClick={this.handleMinusClick}> Minus </button>
+                <ul>
+                    {users.map(user => <li>{user}</li>)}
+                </ul>
             </div>
         );
     }
